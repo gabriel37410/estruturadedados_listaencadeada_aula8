e17ea309e6ebf3a83ae77d5bef5c675369f2bbc6
gabriel37410 <gabriel_37410@yahoo.com>
2020-06-06 12:27:58 -0300

    Implementado funcoes do menu

--- linkedListCodeReview.c
+++ linkedListCodeReview.c
@@ -1,134 +1,169 @@
-// http://hpca23.cse.tamu.edu/taco/utsa-www/ut/utsa/cs1723/lecture4.html
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 #include <locale.h>
-#include <string.h>
 
-//===========================ELEMENTOS DA LISTA===========================
-typedef struct _student {
-	int		  id;
-	char		name[100];
-	float		notaAluno;
-} student;
+typedef struct _estudante {
+    int id;
+	char name[100];
+	float notaAluno;
+} Estudante;
 
-//===========================NÓ DA LISTA ENCADEADA===========================
 typedef struct _node {
-	student		k;
-	struct _node	*next; // ponteiro para o próximo elemento
-} node, *list;
+	Estudante estudante;
+	struct _node *prox;
+	struct _node *ant;
+} Node, *list;
 
-//===========================INSERI OS DADOS NA LISTA===========================
-void insert_list (list *L, student k) {
-	node	*p;
-	p = (node *) malloc (sizeof (node));
-	p->k = k;
-	p->next = *L;
+void insert_list (list *lista_estudante, Estudante estudante) {
+	Node *auxiliar;
 
-	*L = p;
+	auxiliar = (Node *) malloc (sizeof (Node));
+	auxiliar->estudante = estudante;
+
+	if(*lista_estudante == NULL) {
+		*lista_estudante = auxiliar;
+	} else {
+		(*lista_estudante)->ant = auxiliar;
+		auxiliar->prox = *lista_estudante;
+
+		*lista_estudante = auxiliar;
+	}
 }
 
-//===========================PROCURA NA LISTA===========================
-student *search_list (list L, int id) {
-	node	*p;
+Estudante *search_list (list lista_estudante, int id) {
+	Node *auxiliar;
 
-	for (p=L; p && p->k.id != id; p=p->next);
-
-	if (p)
-		return &p->k;
-	else
-		return NULL;
-}
-
-//===========================CRIA UMA LISTA VAZIA===========================
-void create_list (list *L) {
-	*L = NULL;
-}
-
-//===========================OPÇÕES DO MENU===========================
-void mostraMenu() {
-	printf("1 - Procurar elemento na lista\n");
-    printf("2 - Adicionar elemento na lista\n");
-    printf("3 - Remover elemento da lista\n");
-	printf("4 - Exibir lista de forma ordenada\n");
-    printf("0 - Sair\n");
-}
-
-int main () {
-
-	setlocale(LC_ALL, "Portuguese");
+	for (auxiliar = lista_estudante; auxiliar && auxiliar->estudante.id != id; auxiliar=auxiliar->prox);
 	
-	list		C;	/* a class of students */
-	student		s, *p;
-	int		id;
-	FILE		*f;
+	if (auxiliar) {
+		return &auxiliar->estudante;
+	}
+	else {
+		return NULL;
+	}
+}
 
+void create_list (list *lista_estudante) {
+	*lista_estudante = NULL;
+}
 
-	//===========================ABRE E CARREGA O ARQUIVO TXT===========================
-	f = fopen ("./List.txt", "r");
-	if (!f) {
-		perror ("List");
+list lerArquivo() {
+	list lista_estudante;
+	Estudante	estudante;
+	
+	FILE *file;
+
+	file = fopen ("list.txt", "r");
+
+	if (!file) {
+		printf("Erro ao ler o arquivo\n");
 		exit (1);
 	}
 
-	create_list (&C);
+	create_list (&lista_estudante);
 
-	for (;;) {
-		fscanf (f, "%d %s %f\n", &s.id, s.name, &s.notaAluno);
-		if (feof (f))
-      break;
-		insert_list (&C, s);
+	while(!feof(file)) {
+		fscanf (file, "%d %s %f\n", &estudante.id, estudante.name, &estudante.notaAluno);
+		insert_list (&lista_estudante, estudante);
 	}
-	fclose (f);
-	//
 
-	//===========================MENU===========================
-	int menu = -1;
+	fclose (file);
+
+	return lista_estudante;
+}
+
+void buscarPorID() {
+	Estudante *seleciona_estudante;
+	int id = 0;
+
+	list lista_estudante = lerArquivo();
+
+	while(id != -1) {
+		printf ("\n-1 para voltar ao menu.\nDigite o ID do estudante:");
+		scanf ("%d", &id);
+
+		seleciona_estudante = search_list (lista_estudante, id);
+
+		!seleciona_estudante ? printf("\nEstudante nao encontrado\n") : printf("\n%d\t%s\t%0.2f\n", seleciona_estudante->id, seleciona_estudante->name, seleciona_estudante->notaAluno);
+	}
+}
+
+void exibirOrdemDecrescente() {
+	list lista_estudante = lerArquivo();
+	Node *auxiliar;
+	
+	auxiliar = (Node *) malloc(sizeof(Node));
+	*auxiliar = *lista_estudante;
+
+	while(auxiliar) {
+		printf("ID: %i\tName: %s\tNota: %.2f\n", auxiliar->estudante.id, auxiliar->estudante.name, auxiliar->estudante.notaAluno);
+
+		if(auxiliar->prox == NULL) {
+			break;
+		}
+		*auxiliar = *auxiliar->prox;
+	}
+	free(auxiliar);
+}
+
+void exibirOrdemCrescente() {
+	list lista_estudante = lerArquivo();
+	Node *fim;
+	
+	fim = (Node *) malloc(sizeof(Node));
+	*fim = *lista_estudante;
+
+	while(fim->prox != NULL) {
+		*fim = *fim->prox;
+	}
+
+	while(fim) {
+		printf("ID: %i\tName: %s\tNota: %.2f\n", fim->estudante.id, fim->estudante.name, fim->estudante.notaAluno);
+		
+		if(fim->ant == NULL) {
+			break;
+		}
+
+		*fim = *fim->ant;
+	}
+	free(fim);
+}
+
+
+int main() {
+ int menu = -1;
 	do{
-	system("@cls||clear");
-	mostraMenu();
-	printf("Informe a opção: ");
-	scanf ("%d", &menu);
-    system("@cls||clear");
+		printf("1 - Procurar alunos na lista.\n");
+		printf("2 - Listar alunos em ordem crescente.\n");
+		printf("3 - Listar alunos em ordem descrescente.\n");
+		printf("4 - Remover aluno da lista.\n");
+		printf("0 - Sair\n");
+		scanf("%i", &menu);
 
-    switch (menu)
-    {
-    	case 0: break;
-        case 1:
-        	for (;;) {
-				printf ("Enter student ID, -1 to finish: ");
-				scanf ("%d", &id);
+		switch(menu) {
+			case 0: break;
+			case 1:
+				buscarPorID();
+				break;
 
-				if (id == -1)
-       				break;
+			case 2:
+				exibirOrdemCrescente();
+				break;
 
-				p = search_list (C, id);
+			case 3:
+				exibirOrdemDecrescente();
+				break;
 
+			case 4:
+				printf("Opcao temporariamente indisponivel.\n Aperta qualquer tecla para retornar ao menu.\n\n");
+				getch();
+				break;
 
-				if (!p)
-					printf ("ID #%d not found!\n", id);
-				else
-					printf ("%d\t%s\t%0.2f\n", p->id, p->name, p->notaAluno);
-			}
-            break;
-        
-        case 2:
-            //
-            break;
-
-        case 3:
-            //
-            break;
-
-        case 4:
-            //
-            break;
-
-        default:
-            printf("Opção invalida");
-            break;
-    }
+			default:
+				printf("Opcao invalida.\n\n");
+				break;
+		}
 	}while(menu != 0);
 
 	exit (0);
